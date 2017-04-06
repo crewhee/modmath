7fbe817d33e8695550fb77910ad5898e1a8637d3
Crewhee <tony.sudov@gmail.com>
2017-04-06 13:16:46 +0300

    header

---
+++ modmath/lab2.h
@@ -0,0 +1,10 @@
+#ifndef lab2
+#define lab2
+double function_lab_2(double x, double y);
+double analitics(double x);
+double eiler(double x, double y, double h);
+double runge(double x, double y, double h);
+void calculate(double h);
+
+
+#endif /* Header_h */
